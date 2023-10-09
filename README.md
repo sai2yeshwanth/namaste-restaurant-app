f6bc12d9d9da9856dc47ebbdb68ab934b3a661f8
Sai Yeshwanth Muddaragadda <84711595+sai2yeshwanth@users.noreply.github.com>
2023-10-09 11:55:52 +0000

    create applayout and Header componet

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # namaste-restaurant-app
 Building a restaurant page
+
+Page layout
+
+- Header
+   - Logo
+   - Nav Items
+- Body
+    - Search
+    - RestaurantContainer
+    - RestaurantCard
+      -  Img
+      -  Name of Res, Star Rating, cuisine, delery tie
+- Footer
+    - Copyright
+    - Links
+    - Address
+    - Contact
+
+Two types of Export/Import
+
+- Default Export/Import
+
+export default Component; import Component from "path";
+
+- Named Export/Import
+
+export const Component; import {Component} from "path";
--- app.js
+++ app.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 
-const GreetingComponent = () => (
-    <h1>Hello</h1>
-)
-const HeadingComponent = () =>
-(
-    <h1 className='head' >
-        <GreetingComponent />
-        Namaste React Using functional component
-    </h1>
-);
+// header componet
+const HeadingComponent = () => {
+    return (
+        <div className='header'>
+            <div className='logo-container'>
+                <img 
+                className='logo'
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO2LZsFdj8LmrJ3Ixyibv2fVrDn4qex1QheQ&usqp=CAU' />
+            </div>
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>Adout Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
 
-console.log(HeadingComponent);//object
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+//Body componet
+const ApplayOut = () => {
+    return (
+        <div className='app'>
+
+        {/* header */}
+            <HeadingComponent />
+
+        {/* Body */}
+
+        {/* ooter */}
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<ApplayOut />);
