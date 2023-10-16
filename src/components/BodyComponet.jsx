c38ee7616a4a4290aa4d7a1eb8cbc6bdd42505c6
Sai Yeshwanth Muddaragadda <84711595+sai2yeshwanth@users.noreply.github.com>
2023-10-16 06:44:40 +0000

    react hook

--- src/components/BodyComponet.jsx
+++ src/components/BodyComponet.jsx
@@ -1,21 +1,30 @@
-import {RestauranCard} from './RestauranCard'
-import {resData} from '../utils/constants'
+import { RestauranCard } from './RestauranCard'
+import { resData } from '../utils/constants'
+import { useState } from 'react'
 
 //body Componet
 export const BodyComponet = () => {
+    //create state variable
+    const [listOfRestaurants, setListOfRestaurants] = useState(resData)
+    
     return (
         <div className='body-container'>
-            {/* sesrch bar */}
-            <div className='search'>Search</div>
+
+            <div className='filter'>
+                <button className='filter-btn'
+                    onClick={() => {
+                       const filterdRestaurants = listOfRestaurants.filter((item) => item.rating > 4)
+                       setListOfRestaurants(filterdRestaurants )
+
+                    }}
+                >
+                    Top Rated Restaurants
+                </button>
+            </div>
             <div className='res-container'>
-                {
-                    resData.map((item)=> (
-                        <RestauranCard data={item} />
-
-                    )
-                    )
-                }
-
+                {listOfRestaurants.map((item) => (
+                    <RestauranCard key={item.id} data={item} />
+                ))}
             </div>
 
         </div>
